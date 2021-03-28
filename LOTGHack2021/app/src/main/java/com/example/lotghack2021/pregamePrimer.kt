462d3b2ddbb0968262d7430aca5d59e8f7438bb3
Alice Barbe <alice_barbe@gmail.com>
2021-03-28 14:54:27 +0100

    last minute changes - updated pregame primer

--- LOTGHack2021/app/src/main/java/com/example/lotghack2021/pregamePrimer.kt
+++ LOTGHack2021/app/src/main/java/com/example/lotghack2021/pregamePrimer.kt
@@ -2,15 +2,29 @@ package com.example.lotghack2021
 
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class pregamePrimer : AppCompatActivity() {
+    var sports = arrayOf("Rugby", "Football", "Cricket", "Hockey")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pregame_primer)
-        val textView: TextView = findViewById(R.id.textView) as TextView
-        textView.movementMethod = ScrollingMovementMethod()
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+
+        if (spinner != null) {
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item, sports
+            )
+            spinner.adapter = adapter
+        }
+        spinner.setSelection(0)
     }
 }
