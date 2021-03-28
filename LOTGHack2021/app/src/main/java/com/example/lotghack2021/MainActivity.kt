83d853f200a54ecc20bd562e870e0b3a495750c4
Alice Barbe <alice_barbe@gmail.com>
2021-03-28 07:40:42 +0100

    open google maps app instead

--- LOTGHack2021/app/src/main/java/com/example/lotghack2021/MainActivity.kt
+++ LOTGHack2021/app/src/main/java/com/example/lotghack2021/MainActivity.kt
@@ -1,18 +1,56 @@
 package com.example.lotghack2021
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 
 const val EXTRA_MESSAGE = "com.example.lotghack2021.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var latitude: Double = 52.2084083 // set defaults
+    private var longitude: Double = 0.0856367
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location->
+                if (location != null) {
+                    // use your location object
+                    // get latitude , longitude and other info from this
+                    latitude =  location?.latitude
+                    longitude = location?.longitude
+                }
+
+            }
+        println("user coordinates:$latitude, $longitude")
     }
 
     fun pregameView(view: View) {
@@ -31,7 +69,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun nearestAEView(view: View) {
-        val intent = Intent(this, nearestAE::class.java)
+
+        //val intent = Intent(this, nearestAE::class.java)
+        //startActivity(intent)
+
+        val intent = Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse("https://www.google.com/maps/search/hospital/@$latitude,$longitude,12")
+        )
         startActivity(intent)
     }
 }
